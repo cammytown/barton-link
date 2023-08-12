73ba68094362cc16be0bc58c58c1f190dc320663
Cammy <cameronsteele@steelecameron.com>
2023-08-11 22:52:41 -0700

    organize sass better; improve aesthetics slightly

--- dev-setup.sh
+++ dev-setup.sh
@@ -2,6 +2,6 @@
 # Description: Setup development environment for Barton Link
 
 sass \
-	src/excerpts/static/excerpts/excerpts.scss \
+	src/excerpts/static/excerpts/sass/style.scss \
 	src/excerpts/static/excerpts/excerpts.css \
 	--watch
